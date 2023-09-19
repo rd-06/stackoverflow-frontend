38e94b72cf5bf1eba5154a55bf973849bcc950cd
RD <rishav1616dhiman@gmail.com>
2023-09-19 23:52:35 +0530

    added environments files

--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,11 @@ export class QueryService {
 
 
   updateQuery(reqBody: any) {
-    return this.http.post(`http://localhost:4000/update-query`, { ...reqBody });
+    return this.http.post(`${environment.BASE_URL}/update-query`, { ...reqBody });
   }
 
   getAllQueries() {
-    return this.http.get(`http://localhost:4000/get-queries`);
+    return this.http.get(`${environment.BASE_URL}/get-queries`);
   }
 
 }
