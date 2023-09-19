ac88ab20b6a0587342f180c46e7600820d58ff5e
RD <rishav1616dhiman@gmail.com>
2023-09-19 22:06:32 +0530

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { MainComponent } from './modules/main/main.component';
 
 const routes: Routes = [
   {
-    path: ':user', component: MainComponent
+    path: '**', component: MainComponent
   }
 ];
 
